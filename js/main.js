3667e1589a5ab5b1a41f990ab870f0a77c3a9fd3
Keenan Ray <keenan.ray87@gmail.com>
2020-02-12 22:14:52 -0500

    started main.js for nav

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Document ready
+$(function() {
+  const $hamburger = $(".hamburger");
+  const $menu = $("nav");
+  const $menuList = $(".nav-links");
+  const $menuItem = $(".nav-links > li");
+
+let showMenu = false;
+
+$hamburger.click(function{
+    if(!showMenu){
+        
+    }
+
+    }
+})
+
+});
